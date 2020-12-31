f45213d139e8522f473caaf4ed897f9f50a028da
Sorani Riku <sorani.dev@outlook.com>
2020-12-31 17:50:43 +0100

    fixed: key not set in MovieList for iterated content

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,7 +4,7 @@ const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div className='image-container d-flex justify-content-start m-3' key={index}>
 					<img src={movie.Poster} alt='movie'></img>
 				</div>
 			))}
